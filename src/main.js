9206f5b1a6fcdb6adb0638c72f5c1517034ee0d3
vita <the777the7@gmail.com>
2024-10-22 19:38:58 +0900

    main

--- src/main.js
+++ src/main.js
@@ -84,7 +84,7 @@ const Main = () => {
   };
 
   return (
-    <div className="relative w-[100%] h-[800px] bg-gradient-to-b from-white via-[#fefdfa] via-[#FFE9C9] to-[#fff] overflow-hidden">
+    <div className="relative w-[100%] h-[800px] custom-gradient overflow-hidden">
       <div className="relative left-0 top-0 w-[100%] flex flex-col items-center justify-start ">
         
         {/* 3D 모델 */}
@@ -118,7 +118,7 @@ const Main = () => {
         </div>
 
         {/* 달성률, 날짜 표시 */}
-        <div className="rounded-top-[16px] w-full h-full items-center justify-center gap-[5px] py-[20px] bg-[#fff] " style={{ paddingLeft: '2vh', paddingRight: '2vh', paddingBottom: '2vh' }}>
+        <div className="rounded-t-[16px] w-full h-full items-center justify-center gap-[5px] py-[20px] bg-[#fff] " style={{ paddingLeft: '2vh', paddingRight: '2vh', paddingBottom: '2vh' }}>
           <div className="relative flex-col items-start" style={{ paddingBottom: '1vh'}}>
             <div>
               <span className="w-full h-[40px] text-[24px] leading-[24px] tracking-[.01em] font-bold font-[Pretendard] text-[#000] items-start justify-center">{completionRate}% </span>

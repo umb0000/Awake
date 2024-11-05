997fe7af41d062a7630477a9904e918ccc90eb56
byeol <jo020925@naver.com>
2024-11-06 00:30:45 +0900

    anxiety이상한 주소 해결

--- src/Anxiety/Anxiety.js
+++ src/Anxiety/Anxiety.js
@@ -39,20 +39,20 @@ const Anxiety = () => {
           </div>
           <div className="self-stretch h-[1px] shrink-0 bg-[#fff]"></div>
           <div className="w-[95%] h-[166px] shrink-0 flex flex-row items-center justify-center gap-[11px]">
-            <a href='http://112.152.14.116:10201/AnxietyBook'>
+          <a href='http://kwawake.duckdns.org/AnxietyBook'>
             <div className="relative w-[160px] h-[166px] shrink-0 flex">
-              <a href='http://kwawake.duckdns.org/AnxietyBook'><div className="absolute left-0 top-0 w-[160px] h-[166px] flex flex-col items-start justify-end p-[20px] bg-[#ff9800] rounded-[16px]">
+              <div className="absolute left-0 top-0 w-[160px] h-[166px] flex flex-col items-start justify-end p-[20px] bg-[#ff9800] rounded-[16px]">
                 <div className="self-stretch text-[20px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#fff]">불안의 책</div>
                 <div className="self-stretch text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#fff]">불안할 때 보세요</div>
               </div>
               <img className="absolute left-[81px] top-[12px]" width="66" height="78" src={process.env.PUBLIC_URL + "/img/3dicons1_257.png"} alt="3d icon" />
               <img className="absolute left-[77.19%] right-[8.44%] top-[74.1%] bottom-[12.65%]" width="23" height="22" src={process.env.PUBLIC_URL + "/img/별1_258.png"} alt="star" />
             </div>
-            </a>
+          </a>
 
-            <a href='http://112.152.14.116:10201/AnxietyBookIndex'>
+          <a href='http://kwawake.duckdns.org/AnxietyBookIndex'>
             <div className="relative w-[160px] h-[166px] shrink-0 flex">
-              <a href='http://kwawake.duckdns.org/AnxietyBookIndex'>
+              
               <div className="absolute left-0 top-0 w-[160px] h-[166px] flex flex-col items-start justify-end p-[20px] bg-[#f8f8f8] rounded-[16px]">
                 <div className="self-stretch text-[20px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#000]">책갈피</div>
                 <div className="self-stretch text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#000]">불안의 책 스크랩 모음</div>

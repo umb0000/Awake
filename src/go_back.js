f7ef7f997433bcd98c5267721b76aef00a221ef9
byeol12 <jo020925@naver.com>
2024-11-05 16:48:08 +0900

    뒤로가기버튼

---
+++ src/go_back.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { VscArrowLeft } from "react-icons/vsc";
+import './output.css';
+
+const Back = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1); // 이전 페이지로 이동
+  };
+
+  return (
+    <div>
+      {/* 상단 뒤로가기 버튼 */}
+      <div className="fixed left-0 top-0 w-[360px] h-16 px-1 py-2 bg-white z-50 flex justify-start items-center gap-1 shadow-md" style={{ marginBottom: '10px' }}>
+        {/* 버튼 컨테이너 */}
+        <div className="w-12 h-12 flex-col justify-center items-center gap-2.5 inline-flex">
+          <div className="rounded-full justify-center items-center flex">
+            <button onClick={handleBackClick} className="p-2 flex justify-center items-center">
+              <VscArrowLeft size={24} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* 뒤로가기 버튼 하단에 공간을 추가하기 위해 빈 div */}
+      <div className="h-16"></div> {/* 16px의 여백 추가 */}
+    </div>
+  );
+};
+
+export default Back;
--- src/index.js
+++ src/index.js
@@ -26,27 +26,28 @@ import InsultFeedbackExample from './angry/insultPrevent.js'
 import Forest from './forest/forest.js'
 import AnxietyBookIndex from './Anxiety/AnxietyBookIndex.js'
 import Profile from './Profile'
+import Back from './go_back';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        {/* Addtodo 라우트 - 네비게이션 제외 */}
-        <Route path="/addTodo" element={<Addtodo />} />
-        <Route path="/AnxietyBook" element={<AnxietyBook />} />
-        <Route path="/AnxietyBook2/:id" element={<AnxietyBook2 />} />
-        <Route path="/Breath" element={<Breath />} />
-        <Route path="/ConflictResolution" element={<ConflictResolution />} />
-        <Route path="/ConflictBoard" element={<ConflictBoard />} />
-        <Route path="/ConflictBoardadd" element={<ConflictBoardadd />} />
-        <Route path="/trashcan" element={<TrashCan />} />
-        <Route path="/trashcan_i" element={<TrashCan_i />} />
-        <Route path="/move" element={<Move />} />
-        <Route path="/onion" element={<Onion />} />
-        <Route path="/insult" element={<InsultFeedbackExample />} />
-        <Route path="/forest" element={<Forest/>} />
-        <Route path="/AnxietyBookIndex" element={<AnxietyBookIndex/>} />
+        {/* 뒤로가기 버튼이 필요한 페이지 */}
+        <Route path="/AnxietyBook" element={<><Back /><AnxietyBook /></>} />
+        <Route path="/AnxietyBook2/:id" element={<><Back /><AnxietyBook2 /></>} />
+        <Route path="/Breath" element={<><Back /><Breath /></>} />
+        <Route path="/ConflictResolution" element={<><Back /><ConflictResolution /></>} />
+        <Route path="/ConflictBoard" element={<><Back /><ConflictBoard /></>} />
+        <Route path="/ConflictBoardadd" element={<><Back /><ConflictBoardadd /></>} />
+        <Route path="/trashcan" element={<><Back /><TrashCan /></>} />
+        <Route path="/trashcan_i" element={<><Back /><TrashCan_i /></>} />
+        <Route path="/move" element={<><Back /><Move /></>} />
+        <Route path="/onion" element={<><Back /><Onion /></>} />
+        <Route path="/insult" element={<><Back /><InsultFeedbackExample /></>} />
+        <Route path="/forest" element={<><Back /><Forest /></>} />
+        <Route path="/AnxietyBookIndex" element={<><Back /><AnxietyBookIndex /></>} />
 
 
         {/* 네비게이션을 포함한 모든 다른 라우트 */}

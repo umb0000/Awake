31d37ce7d8a359dc6efdbeea8feeaacaf7468339
byeol <jo020925@naver.com>
2024-11-08 22:15:20 +0900

    마음숲 창 반응형 및 여백 통일

--- src/Depression.js
+++ src/Depression.js
@@ -4,20 +4,20 @@ import './output.css';
 const Depression = () => {
   return (
     <div className="relative w-full h-auto bg-[#fff] overflow-hidden">
-      <div className="w-full max-w-md mx-auto flex flex-col items-center justify-start gap-4 bg-[#fff] px-4">
+      <div className="w-[100%] flex flex-col items-center justify-start gap-[8px] bg-[#fff]">
         
-        <div className="w-full h-[63px] flex-shrink-0 flex items-center justify-center bg-[#f5f5f5] rounded-lg">
+        <div className="w-screen h-[63px] flex-shrink-0 flex items-center justify-center bg-[#f5f5f5]">
           <div className="relative w-full max-w-[337px] h-[73px] flex items-center">
-            <div className="absolute left-0 top-0 w-full flex flex-col items-start justify-center gap-1 p-5 rounded-lg">
-              <div className="text-lg font-bold text-[#000]">우울한 마음을 보듬어요</div>
-              <div className="text-xs text-[#79747e]">나의 마음을 들여다봐요</div>
+            <div className="absolute left-0 top-0 w-[337px] h-[73px] flex flex-col items-start justify-center gap-[3px] p-[20px] rounded-[16px]">
+                <div className="w-[266px] text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#000]">우울한 마음을 보듬어요</div>
+                <div className="w-[263px] text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-medium text-[#79747e]">나의 마음을 들여다봐요</div>
+              </div>
+                <img className="absolute right-0 top-2 object-cover" width="41" height="49" src={process.env.PUBLIC_URL + "/img/3dicons1_335.png"} alt="3d icon" />
             </div>
-            <img className="absolute right-0 top-2 object-cover" width="41" height="49" src={process.env.PUBLIC_URL + "/img/3dicons1_335.png"} alt="3d icon" />
-          </div>
         </div>
 
-        <div className="relative w-full h-[166px] flex-shrink-0">
-          <a href="http://kwawake.duckdns.org/trashcan" className="relative block w-full h-full rounded-lg overflow-hidden">
+        <div className="relative w-[95%] h-[166px] flex-shrink-0">
+          <a href="http://kwawake.duckdns.org/trashcan" className="relative block w-full h-full rounded-[16px] overflow-hidden">
             <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-end p-5 bg-[#88d6e7] rounded-lg">
               <div className="text-xl font-bold text-white">감정 쓰레기통</div>
               <div className="text-xs font-semibold text-white">지금 우리에게 필요한 것</div>
@@ -26,8 +26,8 @@ const Depression = () => {
           </a>
         </div>
 
-        <div className="relative w-full h-[166px] flex-shrink-0">
-          <a href="http://kwawake.duckdns.org/move" className="relative block w-full h-full rounded-lg overflow-hidden">
+        <div className="relative w-[95%] h-[166px] flex-shrink-0">
+          <a href="http://kwawake.duckdns.org/move" className="relative block w-full h-full rounded-[16px] overflow-hidden">
             <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-end p-5 bg-[#ff9800] rounded-lg">
               <div className="text-xl font-bold text-white">몸 움직이기</div>
               <div className="text-xs font-semibold text-white">가볍게 하나씩 따라해보세요.</div>
@@ -36,17 +36,19 @@ const Depression = () => {
           </a>
         </div>
 
-        <div className="relative w-full h-[68px] flex-shrink-0">
-          <a href="http://kwawake.duckdns.org/onion" className="relative block w-full h-full rounded-lg overflow-hidden">
-            <div className="w-full h-full flex flex-col items-start justify-center p-3 bg-[#f8f8f8] rounded-lg">
-              <div className="text-lg font-bold text-[#000]">양파양 위로하기</div>
-              <div className="text-xs font-semibold text-[#000]">긍정의 말로 내 마음 속 양파 키우기</div>
+
+        <a href='http://kwawake.duckdns.org/Breath' className="w-[95%] h-[68px] shrink-0 flex flex-col items-start justify-center gap-[1px] py-[10px] px-[20px] bg-[#f8f8f8] rounded-[16px] no-underline">
+            <div className="self-stretch text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#000]">
+            양파양 위로하기
+            </div>
+            <div className="self-stretch text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#000]">
+             양파양을 다독이며 나도 아끼는 연습을 해봐요.
             </div>
           </a>
-        </div>
 
-        <div className="relative w-full h-[68px] flex-shrink-0">
-          <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-center py-3 px-5 bg-[#ffcd63] rounded-lg">
+
+        <div className="relative w-[95%]  h-[68px] flex-shrink-0">
+          <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-center py-3 px-5 bg-[#ffcd63] rounded-[16px]">
             <div className="text-lg font-black text-white">AWAKE 우울 상담 센터</div>
             <div className="text-xs font-semibold text-white">자주 우울하신가요? AWAKE 우울 상담센터</div>
           </div>
--- src/angry/angry.js
+++ src/angry/angry.js
@@ -1,8 +1,8 @@
-import { useState, ReactNode, useRef } from 'react';
+import { useRef } from 'react';
 import '../output.css';
 
 const Angry = () => {
-    const containerRef = useRef(null);
+  const containerRef = useRef(null);
   let isDown = false;
   let startX;
   let scrollLeft;
@@ -28,112 +28,83 @@ const Angry = () => {
     if (!isDown) return;
     e.preventDefault();
     const x = e.pageX - containerRef.current.offsetLeft;
-    const walk = (x - startX) * 2; // 스크롤 속도 조절
+    const walk = (x - startX) * 2; // Scroll speed adjustment
     containerRef.current.scrollLeft = scrollLeft - walk;
   };
 
   return (
-    <div className="relative w-[100%] h-[800px] sbg-[#fff] overflow-hidden font-['Pretendard_Variable']">
-      {/* <div className="absolute left-0 top-[30px] w-[100%] h-[680px] flex flex-col items-center justify-start gap-[10px] py-[9px] px-[10px]">
-        <div className="relative w-[95%] h-[51px] shrink-0 flex flex-col items-start justify-start py-[7px] px-0">
-          <div className="absolute left-0 top-[5px] w-[308px] h-[41px] shrink-0 flex flex-row items-center justify-start gap-[11px] overflow-hidden">
-            <img width="24" height="22" src={process.env.PUBLIC_URL + "/img/Vector1_233.png"} alt="icon" />
-            <div className="text-[24px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-black text-[#237741] text-center whitespace-nowrap">마음숲</div>
-          </div>
-        </div>
-        <div className="self-stretch flex flex-row items-center justify-center gap-[10px] py-0 px-[19px]">
-          <div className="w-[60px] shrink-0 flex flex-col items-center justify-start gap-[2px] py-[10px] px-0">
-            <div className="self-stretch text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#000] text-center">불안</div>
-            <div className="self-stretch h-[2px] shrink-0 bg-[#ff9800]"></div>
-          </div>
-          <div className="w-[60px] shrink-0 flex flex-col items-center justify-start py-[10px] px-0">
-            <div className="self-stretch text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#000] text-center">분노</div>
-          </div>
-          <div className="w-[60px] shrink-0 flex flex-col items-center justify-start py-[10px] px-0">
-            <div className="self-stretch text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#000] text-center">우울</div>
-          </div>
-          <div className="w-[60px] shrink-0 flex flex-col items-center justify-start py-[10px] px-0">
-            <div className="self-stretch text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#000] text-center">MY</div>
-          </div>
-        </div>*/}
+    <div className="relative w-[100%] h-auto bg-[#fff] overflow-hidden  font-['Pretendard_Variable']">
+     <div className="w-[100%] flex flex-col items-center justify-start gap-[8px] bg-[#fff]">
 
-        <div className="w-[100%] flex flex-col items-center justify-start gap-[8px] bg-[#fff]">
-          <div className="self-stretch h-[63px] shrink-0 flex flex-row items-center justify-center bg-[#f5f5f5]">
-            <div className="relative w-[337px] h-[73px] shrink-0 flex">
-              <div className="absolute left-0 top-0 w-[337px] h-[73px] flex flex-col items-start justify-center gap-[3px] p-[20px] rounded-[16px]">
-                <div className="w-[266px] text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#000]">분노한 마음을 서서히 가라앉혀요</div>
-                <div className="w-[263px] text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-medium text-[#79747e]">화가 나도 일단 진정!</div>
-              </div>
-              <img className="absolute left-[278px] top-[14px]" width="35" height="44" src={process.env.PUBLIC_URL + "/img/3dicons1_509.png"} alt="3d icon" />
+      <div className="w-screen h-[63px] flex-shrink-0 flex items-center justify-center bg-[#f5f5f5]">        
+        <div className="relative w-full max-w-[337px] h-[73px] flex items-center">
+            <div className="absolute left-0 top-0 w-[337px] h-[73px] flex flex-col items-start justify-center gap-[3px] p-[20px] rounded-[16px]">
+              <div className="w-[266px] text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#000]">분노한 마음을 서서히 가라앉혀요</div>
+              <div className="w-[263px] text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-medium text-[#79747e]">화가 나도 일단 진정!</div>
             </div>
-          </div>
-          <div className="self-stretch h-[1px] shrink-0 bg-[#fff]"></div>
-          <div className="self-stretch flex flex-row items-center justify-center">
-            <div className="relative w-[95%] h-[166px] shrink-0 flex bg-[#6fddc6] rounded-[16px]">
-              <a href='http://kwawake.duckdns.org/Breath'>
-                <div className="w-[70%] h-[166px] flex flex-col items-start justify-end p-[20px] ">
-                  <div className="self-stretch text-[20px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#fff]">숨 고르기</div>
-                  <div className="self-stretch text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#fff]">열을 내리는 호흡법으로 천천히 진정해봐요.</div>
-                </div>
-              
-                </a>
-                <div className="w-[40%] h-[166px] flex items-center justify-center" style={{ marginLeft: '5px'}}>
-                  <img className="w-[108px] h-[100px]" src={process.env.PUBLIC_URL + "/img/3dicons1_517.png"} alt="3d icon" />
-                </div>
-            </div>
-          </div>
-          <div className="self-stretch flex flex-row items-center justify-center">
-            <div className="relative w-[95%] h-[166px] shrink-0 flex bg-[#6fddc6] rounded-[16px]">
-              <a href='http://kwawake.duckdns.org/ConflictBoard'>
-              <div className="w-full h-full bg-[#f2b0c8] rounded-2xl flex-col justify-end items-start inline-flex">
-                <img src={process.env.PUBLIC_URL + "/img/CB.png"} alt="분노 상담소" />
-              </div>
-                            
-                </a>
-                
-            </div>
-          </div>
-
-          {/*스크롤*/}
-          <div className="w-[95%] h-[166px] flex flex-row items-center justify-left gap-[11px] overflow-x-scroll scroll-smooth no-scrollbar"
-        ref={containerRef}
-        onMouseDown={handleMouseDown}
-        onMouseLeave={handleMouseLeave}
-        onMouseUp={handleMouseUp}
-        onMouseMove={handleMouseMove}>
-        <div className="relative w-[175px] h-[166px] shrink-0 flex">
-        <a href='http://kwawake.duckdns.org/ConflictResolution'>
-          <div className="absolute left-0 top-0 w-[175px] h-[166px] flex flex-col items-start justify-end p-[20px] bg-[#fe776d] rounded-[16px]">
-            <div className="self-stretch text-[20px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#fff]">갈등 대처법</div>
-            <div className="self-stretch text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#fff]">갈등 해결 with '상감이결'</div>
-          </div>
-          <img className="absolute left-[81px] top-[22px]" width="66" height="78" src={process.env.PUBLIC_URL + "/img/3dicons1_524.png"} alt="3d icon" />
-          </a>
-        </div>
-
-      
-        {/* 추가된 비판 대처법 */}
-        <div className="relative w-[175px] h-[166px] shrink-0 flex">
-        <a href='http://kwawake.duckdns.org/insult'>
-          <div className="absolute left-0 top-0 w-[175px] h-[166px] flex flex-col items-start justify-end p-[20px] bg-[#b269ff] rounded-[16px]">
-            <div className="self-stretch text-[20px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#fff]">비판 대처법</div>
-            <div className="self-stretch text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#fff]">비판 대처표</div>
-          </div>
-          <img className="absolute left-[88px] top-[22px]" width="67" height="78" src={process.env.PUBLIC_URL + "/img/3dicons1_535.png"} alt="3d icon" />
-          </a>
+              <img className="absolute right-0 top-2 object-cover" width="41" height="49" src={process.env.PUBLIC_URL + "/img/3dicons1_509.png"} alt="3d icon" />
         </div>
       </div>
+      
 
-          <div className="relative w-[95%] h-[68px] shrink-0 flex">
-            <div className="absolute left-0 top-0 w-[100%] h-[68px] flex flex-col items-start justify-center py-[10px] px-[20px] bg-[#ffcd63] rounded-[16px]">
-              <div className="self-stretch text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-black text-[#fff]">AWAKE 분노 상담 센터</div>
-              <div className="self-stretch text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-semibold text-[#fff]">상담이 필요하신가요? AWAKE 불안 상담센터</div>
+       <div className="self-stretch h-[1px] shrink-0 bg-[#fff]"></div>
+       
+
+       
+       <div className="relative w-[95%] h-[166px] flex-shrink-0">
+          <a href="http://kwawake.duckdns.org/Breath" className="relative block w-full h-full rounded-[16px] overflow-hidden">
+            <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-end p-5 bg-[#6fddc6] rounded-lg">
+              <div className="text-xl font-bold text-white">숨 고르기</div>
+              <div className="text-xs font-semibold text-white">열을 내리는 호흡법으로 천천히 진정해봐요.</div>
             </div>
-            <div className="absolute left-[203px] top-[6px] w-[124px] text-[50px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-black text-[#fff] opacity-[.3]">AWA</div>
+            <img className="absolute right-5 top-3 object-cover" width="108" height="100" src={process.env.PUBLIC_URL + "/img/3dicons1_517.png"} alt="3d icon" />
+          </a>
+        </div>
+
+        <div className="relative w-[95%]  h-[166px] flex-shrink-0">
+          <a href="http://kwawake.duckdns.org/ConflictBoard" className="relative block w-full h-full rounded-[16px] overflow-hidden">
+            <div className="absolute left-0 top-0 w-full h-full flex items-center justify-center bg-[#f2b0c8] rounded-lg">
+              <img src={process.env.PUBLIC_URL + "/img/CB.png"} alt="분노 상담소" />
+            </div>
+          </a>
+        </div>
+
+        {/* Scrollable section */}
+        <div className="w-[95%]  h-[166px] flex flex-row items-center justify-start gap-4 overflow-x-scroll scroll-smooth no-scrollbar"
+          ref={containerRef}
+          onMouseDown={handleMouseDown}
+          onMouseLeave={handleMouseLeave}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}>
+          <div className="relative w-[175px] h-[166px] flex-shrink-0 flex">
+            <a href="http://kwawake.duckdns.org/ConflictResolution" className="relative block w-full h-full rounded-[16px] overflow-hidden">
+              <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-end p-5 bg-[#fe776d] rounded-lg">
+                <div className="text-xl font-bold text-white">갈등 대처법</div>
+                <div className="text-xs font-semibold text-white">갈등 해결 with '상감이결'</div>
+              </div>
+              <img className="absolute left-[81px] top-[22px]" width="66" height="78" src={process.env.PUBLIC_URL + "/img/3dicons1_524.png"} alt="3d icon" />
+            </a>
+          </div>
+
+          <div className="relative w-[175px] h-[166px] flex-shrink-0 flex">
+            <a href="http://kwawake.duckdns.org/insult" className="relative block w-full h-full rounded-[16px] overflow-hidden">
+              <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-end p-5 bg-[#b269ff] rounded-lg">
+                <div className="text-xl font-bold text-white">비판 대처법</div>
+                <div className="text-xs font-semibold text-white">비판 대처표</div>
+              </div>
+              <img className="absolute left-[88px] top-[22px]" width="67" height="78" src={process.env.PUBLIC_URL + "/img/3dicons1_535.png"} alt="3d icon" />
+            </a>
           </div>
         </div>
-      
-      
+
+        <div className="relative w-[95%]  h-[68px] flex-shrink-0">
+          <div className="absolute left-0 top-0 w-full h-full flex flex-col items-start justify-center py-3 px-5 bg-[#ffcd63] rounded-[16px]">
+            <div className="text-lg font-black text-white">AWAKE 분노 상담 센터</div>
+            <div className="text-xs font-semibold text-white">상담이 필요하신가요? AWAKE 불안 상담센터</div>
+          </div>
+          <div className="absolute right-5 top-1 w-[124px] text-[50px] font-black text-white opacity-30">AWA</div>
+        </div>
+        </div>
     </div>
   );
 };

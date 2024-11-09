8a031c24e070f1f7650fbc84894d35a5b3657f43
byeol <jo020925@naver.com>
2024-11-10 04:31:14 +0900

    그만하라했다경찰부른다진짜

--- src/index.js
+++ src/index.js
@@ -60,25 +60,6 @@ root.render(
         <Route path="/insult" element={<><Back /><InsultFeedbackExample /></>} />
         <Route path="/forest" element={<><Back /><Forest /></>} />
         <Route path="/AnxietyBookIndex" element={<><Back /><AnxietyBookIndex /></>} />
-<<<<<<< HEAD
-
-        {/* Main 페이지에 LevelUpPopup을 포함하도록 Route 설정 */}
-        <Route path="/" element={<Main />} />
-        <Route path="/main" element={<Main />} />
-
-        {/* 기타 페이지 */}
-        <Route path="/Kit" element={<Kit />}>
-          <Route path="Anxiety" element={<Anxiety />} />
-          <Route path="Angry" element={<Angry />} />
-          <Route path="Depression" element={<Depression />} />
-        </Route>
-        <Route path="/Anxiety" element={<Anxiety />} />
-        <Route path="/Angry" element={<Angry />} />
-        <Route path="/Depression" element={<Depression />} />
-        <Route path="/calender" element={<Calender />} />
-        <Route path="/profile" element={<Profile />} />
-
-=======
         <Route path="/Help" element={<><Back /><Help /></>} />
         <Route path="/HelpList" element={<><Back /><HelpList /></>} />
         <Route path="/HelpListCheck" element={<><Back /><HelpListCheck /></>} />
@@ -110,7 +91,6 @@ root.render(
             </div>
           }
         />
->>>>>>> a310d3ce1f4001f065e194a08e4d58ff8e20fdbb
       </Routes>
 
       {/* Navigation은 모든 페이지에서 표시 */}

207da536c2d1da88c01a8a08583661463abab9dd
byeol <jo020925@naver.com>
2024-11-12 21:59:15 +0900

    편지지

--- src/main.js
+++ src/main.js
@@ -201,15 +201,17 @@ const Main = () => {
       <div className="relative left-0 top-0 w-[100%] flex flex-col items-center justify-start ">
 
        
+        
+        
         {/* 오른쪽 상단 작은 이미지 */}
         <a
           href="http://kwawake.duckdns.org/collect"
           className="absolute top-12 right-4 w-[50px] h-[50px] flex items-center justify-center z-10" // z-index 추가
         >
           <img
-            src={process.env.PUBLIC_URL + "/img/mail.png"}
-            alt="Small Icon"
-            className="w-[35px] h-[35px] transform rotate-[15deg]"
+            src={process.env.PUBLIC_URL + "/img/dogam.png "}
+            alt="dogam"
+            className="w-[30px] h-[25px] transform rotate-[15deg]"
           />
         </a>
         {/* 오른쪽 상단 작은 이미지 */}
@@ -218,12 +220,11 @@ const Main = () => {
           className="absolute top-24 right-4 w-[50px] h-[50px] flex items-center justify-center z-10" // z-index 추가
         >
           <img
-            src={process.env.PUBLIC_URL + "/img/dogam.png"}
+            src={process.env.PUBLIC_URL + "/img/mail.png"}
             alt="Small Icon"
-            className="w-[35px] h-[35px] transform rotate-[15deg]"
+            className="w-[35px] h-[35px] shadow-sm"
           />
         </a>
-
 
 
         {/* 3D 모델 표시 영역 */}

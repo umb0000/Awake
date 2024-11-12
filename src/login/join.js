cd1d0fd1d429a9577626c9eaba07513e7f91fae1
vita <the777the7@gmail.com>
2024-11-13 02:44:07 +0900

    join

--- src/login/join.js
+++ src/login/join.js
@@ -245,27 +245,91 @@ function Join({ onRegisterSuccess, onSwitchToLogin }) {
           </div>
             
           )}
+
+{currentStep === 6 && (
+            <div className="w-[360px] h-screen bg-white relative pt-20">
+           <div className="absolute top-20 left-6 flex gap-2">
+        <div className="w-[11px] h-[11px] bg-[#d9d9d9] rounded-full" />
+        <div className="w-[11px] h-[11px] bg-[#ff6d00] rounded-full" />
+        <div className="w-[11px] h-[11px] bg-[#d9d9d9] rounded-full" />
+      </div>
+
+      {/* 콘텐츠 */}
+      <div className="flex flex-col justify-start items-start gap-5 px-6 pt-16">
+        <div>
+          <span className="text-black text-[23px] font-bold font-['Pretendard'] leading-[30px] tracking-tight">사소한, 갑작스러운, 만성적인<br/></span>
+          <span className="text-[#ff6d00] text-[23px] font-bold font-['Pretendard'] leading-[30px] tracking-tight">감정</span>
+          <span className="text-black text-[23px] font-bold font-['Pretendard'] leading-[30px] tracking-tight">을 들려주세요</span>
+        </div>
+
+        {/* 박스 이미지와 마음숲 텍스트 */}
+        <div className="flex items-center gap-2">
+          <img src="/img/kitbox.png" alt="Box Icon" className="w-6 h-6" />
+          <div className="text-[#ff6d00] text-2xl font-black font-['Pretendard'] leading-normal tracking-wide">
+            마음숲
+          </div>
+        </div>
+
+        <div className="self-stretch text-[#1d1b20] text-xs font-medium font-['Pretendard'] leading-[18px] tracking-tight">
+          바쁘고 걱정이 많은 현대 사회, <br/>마음이 정리되지 않으면 할 일을 할 수 없어요.<br/>
+          10여 가지의 CBT (인지행동치료, Cognitive behavioral therapy) 방법으로 탄생한 어웨이크의 마음숲에서 <br/>당신의 감정을 들려주세요.
+        </div>
+
+        <img className="w-[307px] h-[351px]" src="/img/book.png" alt="CBT-based Emotional Support Graphic" />
+      </div>
+          </div>
+            
+          )}
+
+{currentStep === 7 && (
+            <div className="w-[360px] h-screen bg-white relative pt-20">
+            <div className="absolute top-20 left-6 flex gap-2">
+        <div className="w-[11px] h-[11px] bg-[#d9d9d9] rounded-full" />
+        <div className="w-[11px] h-[11px] bg-[#d9d9d9] rounded-full" />
+        <div className="w-[11px] h-[11px] bg-[#ff6d00] rounded-full" />
+      </div>
+
+      <div className="flex flex-col justify-start items-start gap-5 px-6 pt-16">
+        <div>
+          <span className="text-black text-[23px] font-bold font-['Pretendard'] leading-[30px] tracking-tight">레벨업하며<br/>더 많은 </span>
+          <span className="text-[#ff6d00] text-[23px] font-bold font-['Pretendard'] leading-[30px] tracking-tight">웨이냥</span>
+          <span className="text-black text-[23px] font-bold font-['Pretendard'] leading-[30px] tracking-tight">을 모아보세요!</span>
+        </div>
+
+        <div className="self-stretch text-black text-xs font-medium font-['Pretendard'] leading-[18px] tracking-tight">
+          당신의 러닝메이트,<br />어웨이크 웨이냥들이 당신을 기다리고 있어요!
+        </div>
+
+        <img className="w-[359px] h-[350px]" src="/img/catcatcat.png" alt="Level up rewards image" />
+      </div>
+          </div>
+            
+          )}
                 </div>
 
                 <button
-            type={currentStep === 3 ? "submit" : "button"}
-            onClick={handleNextClick}
-            disabled={
-                (currentStep === 1 && !isNicknameValid) ||
-                (currentStep === 2 && !isEmailValid) ||
-                (currentStep === 3 && !isPasswordValid)
-            }
-            className={`w-full py-3 mt-6 absolute bottom-0 left-0 flex items-center justify-center py-[17px] px-[113px] ${
-                (currentStep === 1 && isNicknameValid) ||
-                (currentStep === 2 && isEmailValid && !isEmailDuplicate) ||
-                (currentStep === 3 && isPasswordValid) ||
-                (currentStep >= 4)
-                    ? 'bg-[#ff6d00]'
-                    : 'bg-gray-300'
-            } text-white font-semibold z-20`}
-        >
-            {currentStep === 4 ? "어웨이크 시작하기" : "다음"}
-        </button>
+    type={currentStep === 3 ? "submit" : "button"}
+    onClick={handleNextClick}
+    disabled={
+        (currentStep === 1 && !isNicknameValid) ||
+        (currentStep === 2 && !isEmailValid) ||
+        (currentStep === 3 && !isPasswordValid)
+    }
+    className={`w-full py-3 mt-6 absolute bottom-0 left-0 flex items-center justify-center py-[17px] px-[113px] ${
+        (currentStep === 1 && isNicknameValid) ||
+        (currentStep === 2 && isEmailValid && !isEmailDuplicate) ||
+        (currentStep === 3 && isPasswordValid) ||
+        (currentStep >= 4)
+            ? 'bg-[#ff6d00]'
+            : 'bg-gray-300'
+    } text-white font-semibold z-20`}
+>
+    {currentStep === 4 
+        ? "어웨이크 시작하기" 
+        : currentStep === 7 
+        ? "로그인 하러 가기" 
+        : "다음"}
+</button>
             </form>
         </div>
     );

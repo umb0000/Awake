47d9b9349156cda1ae4caa2700c3cea0b7968bcc
vita <the777the7@gmail.com>
2024-11-12 21:26:10 +0900

    test

--- src/maintodoCard.js
+++ src/maintodoCard.js
@@ -31,72 +31,70 @@ const Card = ({ card, onCheck, onDelete }) => {
 
   return (
     <motion.div
-      key={card.id}
-      className="relative w-full h-[50px] flex"
-      drag="x"
-      dragConstraints={{ left: 0, right: 0 }}
-      onDragEnd={(event, info) => {
+    key={card.id}
+    className="relative w-full h-[60px] flex"
+    drag="x"
+    dragConstraints={{ left: 0, right: 0 }}
+    onDragEnd={(event, info) => {
         if (info.offset.x < -100) {
-          setIsDragged(true); // 왼쪽으로 드래그되면 X 버튼 보이기
+            setIsDragged(true); // Show delete button when dragged left
         } else {
-          setIsDragged(false); // 오른쪽으로 다시 드래그하면 X 버튼 숨기기
+            setIsDragged(false); // Hide delete button when dragged right
         }
-      }}
-      animate={{ x: isDragged ? -60 : 0 }} // isDragged에 따라 위치 조정
-      transition={{ type: "spring", stiffness: 300 }}
-      layout
-      style={{ height: '10vh' }}
-    >
-<div className="absolute left-0 top-0 w-[100%] h-[50px] bg-[#f4f7f8] rounded-[10px]"></div>
-<div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 w-[330px] h-[60px]"></div>
-
-        
-      <div className="absolute left-0 top-0 w-full h-full bg-[#f4f7f8] rounded-[10px] item-center">
-      <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 w-[294px] flex flex-row items-center gap-[10px] ">
+    }}
+    animate={{ x: isDragged ? -60 : 0 }}
+    transition={{ type: "spring", stiffness: 300 }}
+    layout
+>
+    <div className="absolute left-0 top-0 w-full h-full bg-[#f4f7f8] rounded-[10px] flex items-center px-4">
+        {/* Icon */}
         <img
-          width="30"
-          height="31"
-          src={`${process.env.PUBLIC_URL}/img/${card.image}`}
-          alt={`level${card.level}`}
+            width="30"
+            height="31"
+            src={`${process.env.PUBLIC_URL}/img/${card.image}`}
+            alt={`level${card.level}`}
+            className="mr-2"
         />
 
-        <div className="h-[30px] flex-grow flex flex-col items-start justify-center" style={{ minWidth: '73%', maxWidth: '180px' }}>
-          <div className="w-full text-[13px] leading-[20px] tracking-[.01em] font-['Pretendard'] font-semibold text-[#79747e] flex flex-col justify-center">
-            {card.title}
-          </div>
-          <div className="w-full h-[2vh] text-[10px] leading-[10px] tracking-[.01em] font-['Pretendard'] text-[#79747e] flex flex-col justify-center">
-            {card.detail}
-          </div>
-        </div>
+        {/* Title and Details */}
+        <div className="flex-grow">
+            <div className="text-[13px] font-semibold text-[#79747e]">
+                {card.title}
+            </div>
+            <div className="text-[10px] text-[#79747e]">
+                {card.detail}
+            </div>
         </div>
 
-        {isDragged && (
-          <motion.button
+        {/* Checkbox */}
+        <img
+            width="26"
+            height="26"
+            src={`${process.env.PUBLIC_URL}/img/${card.checked ? 'checked.png' : 'unchecked.png'}`}
+            alt={card.checked ? 'checked' : 'unchecked'}
+            onClick={(e) => {
+                e.stopPropagation();
+                onCheck(card.id);
+            }}
+            className="ml-2"
+        />
+    </div>
+
+    {/* Delete Button */}
+    {isDragged && (
+        <motion.button
             className="absolute right-[-70px] transform -translate-y-1/2 bg-red-500 text-white px-2 py-1 rounded"
             onClick={handleDeleteClick}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
-          >
+        >
             X
-          </motion.button>
-        )}
-
-        <img
-          width="26"
-          height="26"
-          src={`${process.env.PUBLIC_URL}/img/${card.checked ? 'checked.png' : 'unchecked.png'}`}
-          alt={card.checked ? 'checked' : 'unchecked'}
-          onClick={(e) => {
-            e.stopPropagation();
-            onCheck(card.id);
-          }}
-          className="shrink-0"
-        />
-      </div>
-    </motion.div>
-  );
+        </motion.button>
+    )}
+</motion.div>
+);
 };
 
 export default Card;

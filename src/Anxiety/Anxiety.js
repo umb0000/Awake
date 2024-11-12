a05bd060426dd833cd0ff50c6b30d3ed05ca1a6d
byeol <jo020925@naver.com>
2024-11-13 01:12:55 +0900

    불안상단바없어진거얺음

--- src/Anxiety/Anxiety.js
+++ src/Anxiety/Anxiety.js
@@ -5,7 +5,17 @@ const Anxiety = () => {
   return (
     <div className="relative w-[100%] h-[800px] bg-[#fff] overflow-hidden font-['Pretendard_Variable']">
       <div className="w-[100%] flex flex-col items-center justify-start gap-[8px] bg-[#fff]">
-        
+
+      <div className="w-screen h-[63px] flex-shrink-0 flex items-center justify-center bg-[#f5f5f5]">        
+        <div className="relative w-full max-w-[337px] h-[73px] flex items-center">
+            <div className="absolute left-0 top-0 w-[337px] h-[73px] flex flex-col items-start justify-center gap-[3px] p-[20px] rounded-[16px]">
+              <div className="w-[266px] text-[16px] leading-[24px] tracking-[.01em] font-['Pretendard_Variable'] font-bold text-[#000]">불안하고 초조한 마음을 치유해요</div>
+              <div className="w-[263px] text-[11px] leading-[10px] tracking-[.01em] font-['Pretendard_Variable'] font-medium text-[#79747e]">침착하게 나의 불안에 대해 생각해봐요</div>
+            </div>
+            <img className="absolute left-[292px] top-[15px]" width="26" height="43" src={process.env.PUBLIC_URL + "/img/3dicons1_250.png"} alt="3d icon" />
+        </div>
+      </div>
+
         {/* 불안의 책 링크 */}
         <div className="w-[95%] h-[166px] flex flex-row items-center justify-center gap-[10px]">
           <a href="http://kwawake.duckdns.org/AnxietyBook" className="relative w-[166px] h-[166px] flex">

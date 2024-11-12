1df36cd96a9334ff5198de39540672479e983b03
unknown <rlawldus8642@naver.com>
2024-11-12 23:58:01 +0900

    call

--- src/center.js
+++ src/center.js
@@ -14,61 +14,85 @@ const Center = () => {
         </div>
         <div className="w-[360px] h-[704px] flex-col justify-start items-center gap-[15px] flex">
             <div className="w-[338px] h-[639px] flex-col justify-start items-start gap-[5px] flex">
-                <div className="h-[58px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-[#ff7936] rounded-[10px]" />
-                    <div className="w-[313.03px] left-[23px] top-[17px] absolute text-white text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">자살예방 상담 전화</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/whitecall.png" />
-                </div>
-                <div className="h-[58px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">서울광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">서울광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">부산광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">대구광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">인천광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">광주광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">대전광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">울산광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">세종광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
-                <div className="self-stretch h-[55px] relative">
-                    <div className="w-[338px] h-[55px] left-0 top-0 absolute rounded-[10px] border border-[#ff7936]" />
-                    <div className="w-[272.28px] left-[21.72px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">경기도광역정신건강복지센터</div>
-                    <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" />
-                </div>
+            <div className="h-[58px] relative">
+          <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-[#ff7936] rounded-[10px]" />
+          <div className="w-[313.03px] left-[23px] top-[17px] absolute text-white text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">자살예방 상담 전화</div>
+          <a href="tel:1234567890">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/whitecall.png" alt="전화 연결" />
+          </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">서울광역정신건강복지센터</div>
+            <a href="tel:0234449934">
+             <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">부산광역정신건강복지센터</div>
+            <a href="tel:0512422575">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">대구광역정신건강복지센터</div>
+            <a href="tel:0532560199">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">인천광역정신건강복지센터</div>
+            <a href="tel:0324312979">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">광주광역정신건강복지센터</div>
+            <a href="tel:0626001930">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+        </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">대전광역정신건강복지센터</div>
+            <a href="tel:0428637700">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">울산광역정신건강복지센터</div>
+            <a href="tel:0527167190">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">세종광역정신건강복지센터</div>
+            <a href="tel:0448654597">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
+
+        <div className="h-[58px] relative">
+            <div className="w-[338px] h-[55px] left-0 top-0 absolute bg-white rounded-[10px] border border-[#ff7936]" />
+            <div className="w-[313.03px] left-[23px] top-[17px] absolute text-black text-sm font-semibold font-['Pretendard'] leading-tight tracking-wide">경기도광역정신건강복지센터</div>
+            <a href="tel:0312120435">
+            <img className="w-10 h-10 left-[290px] top-[7px] absolute" src="/img/blackcall.png" alt="전화 연결" />
+            </a>
+        </div>
             </div>
         </div>
     </div>

ae6dd1f615a994f3d581684e2e5d466da7e60b96
byeol <jo020925@naver.com>
2024-11-13 09:46:18 +0900

    말풍선

--- src/main.js
+++ src/main.js
@@ -251,7 +251,7 @@ const Main = () => {
       <div className="relative left-0 top-0 w-[100%] flex flex-col items-center justify-start ">
 
      {/* 말풍선 */}
-      <div className="absolute top-12 left-4 bg-white text-white px-1 py-1 rounded-lg shadow-lg max-w-[100px] h-[30px] flex items-center justify-center text-center  break-all whitespace-pre-line z-10">
+      <div className="absolute top-12 left-4 bg-white text-white px-2 py-1 rounded-lg shadow-lg max-w-[100px] h-[40px] flex items-center justify-center text-center  break-all whitespace-pre-line z-10">
           <span className="text-[10px] text-[#4B4B4B] font-['Pretendard_Variable'] font-bold">{displayText}</span>
       </div>
 

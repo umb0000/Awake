ca50f0a60485f7dbc6b8cbf7754559d9cf09e6ee
vita <the777the7@gmail.com>
2024-11-13 23:54:57 +0900

    ss

--- src/main.js
+++ src/main.js
@@ -339,7 +339,7 @@ const Main = () => {
         <div className="rounded-t-[30px] w-full h-full items-center justify-center gap-[5px] py-[20px] bg-[#fff]" style={{ paddingLeft: '2vh', paddingRight: '2vh', paddingBottom: '2vh' }}>
         <div className="relative flex-col items-start">
             <div>
-              <span className="w-full h-[40px] text-[24px] leading-[24px] tracking-[.01em] font-bold font-[Pretendard] text-[#000] items-start justify-center">{((completedCards / totalCards) * 100)}% </span>
+              <span className="w-full h-[40px] text-[24px] leading-[24px] tracking-[.01em] font-bold font-[Pretendard] text-[#000] items-start justify-center">{((completedCards / totalCards) * 100).toFixed(1)}% </span>
               <span className='font-[Pretendard] font-bold text-[13px] text-[#79747e]'>{completedCards}/{totalCards}</span>
             </div></div>
 

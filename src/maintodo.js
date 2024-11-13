473edd2cac6405aca8b0e94c51e1c890377eeba2
vita <the777the7@gmail.com>
2024-11-14 06:41:11 +0900

    todo

--- src/maintodo.js
+++ src/maintodo.js
@@ -201,7 +201,7 @@ const TodoList = ({ onCompletionRateChange, onPointChange, onCheck, onUpdateCatD
       });
 
      // onCheck(card);
-      //fetchTodos();
+      fetchTodos();
     } catch (error) {
       console.error('Error updating todo status:', error);
     }
